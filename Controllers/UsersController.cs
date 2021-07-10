fee4a6b11149fd1569492a0e953e30e16266f2d8
sriravi-1987 <sriram.ravi1002@gmail.com>
2021-07-11 00:06:53 +0530

    https://github.com/sriravi-1987/API

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,12 +17,14 @@ namespace API.Controllers
             _context = context;
         }
 
-        [HttpGet]        
+        [HttpGet]      
+        [AllowAnonymous]  
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
+        [Authorize]
         // api/users/3
         [HttpGet("{id}")]       
         public async Task<ActionResult<AppUser>> GetUser(int id)
